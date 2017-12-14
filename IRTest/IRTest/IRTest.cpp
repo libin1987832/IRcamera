2fc142f687a17564aa9361a9960c5108dcadd631
libin <libin@anbot.cn>
2017-12-14 12:16:43 +0800

    feature:add temprature

--- IRTest/IRTest/IRTest.cpp
+++ IRTest/IRTest/IRTest.cpp
@@ -20,6 +20,7 @@ using namespace std;
 typedef int (*Dllfun)(const char* strIpAddr, HWND hWnd, H264STREAMRECV_CB h264StreamCallBack, LIVEVIDEOSTREAMRGBRECV_CB liveVideoStreamRGBCallback, LIVEVIDEOMEASUREDATARECV_CB liveVideoMeasureDataCallBack, LIVEVIDEOSTREAMSTATERECV_CB liveVideoStreamStateCallBack, LIVEVIDEOMEASURESTATERECV_CB liveVideoMeasureStateCallBack);
 typedef int (*Dllfun2)(const char* strIpAddr, HWND hWnd, int palIndex, LIVEVIDEORGBANDMEASUREDATARECV_CB liveVideoRGBAndMeasureDataCallBack, LIVEVIDEOMEASURESTATERECV_CB liveVideoMeasureStateCallBack);
 typedef int (*Dllfun3)(int iSessionID);
+typedef int (*Dllfun4)(const char* filename, int iSessionID);
 bool stop =true;
 Mat contour(Mat& image)
 {
@@ -69,20 +70,64 @@ Mat contour(Mat& image)
 }
 int CALLBACK rgbcallback(const LiveStream_RGBAndMeasureDataCallBack *pInfo)
 {
+	//return 0;
 	//printf("picture %d",);
 	static int index=0;
 	printf("picture w(%d),h(%d),size(%d,%d),%d",pInfo->ImgWidth,pInfo->ImgHeight,pInfo->RGBDataLen,3*pInfo->ImgWidth*pInfo->ImgHeight,pInfo->matrixDataLen);
+			int row=pInfo->ImgHeight;
+		int col=pInfo->ImgWidth;
+	Mat mask=Mat::zeros(row,col,CV_8U);
+	char imageName[100];
+		sprintf(imageName, "image/i_%d.jpg", index++);
+	if(pInfo->MeasureMatrix!=NULL)
+	{
+
+		//float* mem = new float[pInfo->ImgWidth*pInfo->ImgHeight];
+		//memcpy(mem, pInfo->MeasureMatrix, pInfo->ImgWidth*pInfo->ImgHeight);
+		/*Mat src(row,col,CV_32F,mem);
+		float belt=255.0f/170.0f;
+		float alph=20.0f*belt;
+		Mat m;
+		printf("tem:%f,%f\n",pInfo->MeasureMatrix[pInfo->ImgWidth*200+200],src.at<float>(200,200));
+		src.convertTo(m,CV_8U,belt,alph);
+		uchar uu=m.at<uchar>(200,200);
+		printf("tem:%d\n,%d",(int)uu,sizeof(float));*/
+		float belt=255.0f/170.0f;
+		for(int i=0;i<row;i++)
+		{
+			for(int j=0;j<col;j++)
+			{
+				if(pInfo->MeasureMatrix[i*col+j]>20&&pInfo->MeasureMatrix[i*col+j]<40)
+				{
+					mask.at<uchar>(i,j)=255;
+					//mask.at<uchar>(i,j)=(pInfo->MeasureMatrix[i*col+j]+20)*belt;
+				}
+				else
+					mask.at<uchar>(i,j)=0;
+			}
+		}
+		//printf("tem:%f,%d\n",pInfo->MeasureMatrix[pInfo->ImgWidth*200+200],(int)mask.at<uchar>(200,200));
+		//imwrite(imageName,mask);
+		//threshold(m,mask,-10.0f*belt+alph,255,THRESH_TOZERO);
+		//threshold(mask,mask,130.0f*belt+alph,255,THRESH_TOZERO_INV);
+		//threshold(mask,mask,-10.0f*belt+alph,255,THRESH_BINARY);
+		//imwrite(imageName,mask);
+		//delete [] mem;
+		//return 1;
+	}
 	if(pInfo->pRGBData != NULL)
 	{		
-		char imageName[100];
-		sprintf(imageName, "image/i_%d.jpg", index++);
+		
+	
 		BYTE* mem = new BYTE[3*pInfo->ImgWidth*pInfo->ImgHeight];
-
 		memcpy(mem, pInfo->pRGBData, 3*pInfo->ImgWidth*pInfo->ImgHeight);
-		int row=pInfo->ImgHeight;
-		int col=pInfo->ImgWidth;
-		Mat src(row,col,CV_8UC1,mem);
-		imwrite(imageName,src);
+		Mat rgbsrc(row,col,CV_8UC3,mem);
+		//imshow("windows1",src);
+		//waitKey(1);
+		//contour(src);
+		Mat ss(row,col,CV_8UC3);
+		rgbsrc.copyTo(ss,mask);
+		imwrite(imageName,ss);
 		delete [] mem;
 		//imshow("aa",src);
 		//waitKey(1);
@@ -117,8 +162,11 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 			// TODO: 在此处为应用程序的行为编写代码。
 			Dllfun2 maopao1;
 			Dllfun3 maopao3;
+			Dllfun4 maopao4;
 			HINSTANCE hdll;
 			//cv::namedWindow("aa");
+			//cv::namedWindow("windows1",0); //创建一个窗口 
+			//HWND hWnd = (HWND)cvGetWindowHandle("windows1");//获取子窗口的HWND
 			hdll=LoadLibrary(L"RobotInspection.dll");
 			if(hdll==NULL)
 			{
@@ -130,6 +178,10 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 			LIVEVIDEOMEASURESTATERECV_CB stat=statscallback;
 			int iSessionID=maopao1("192.168.1.168",NULL,1,rgb,stat);
 			printf("iSessionID=%d",iSessionID);
+			//Sleep(10000);
+			//maopao4=(Dllfun4)GetProcAddress(hdll,"LiveStream_Slap");
+			//maopao4("image/aa.jpg",iSessionID);
+			printf("/n/n/n/ndsaf");
 			char input=' ';
 			scanf("%c",&input);
 			maopao3(iSessionID);
