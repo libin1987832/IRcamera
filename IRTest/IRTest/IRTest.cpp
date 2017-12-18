f0ac5fab166f8d3334c254e5bf260d742055858f
d <you@example.com>
2017-12-18 14:42:32 +0800

    bug:maybe delete

--- IRTest/IRTest/IRTest.cpp
+++ IRTest/IRTest/IRTest.cpp
@@ -1,8 +1,6 @@
-// IRTest.cpp : 定义控制台应用程序的入口点。
-//
 
 #include "stdafx.h"
-#include "IRTest.h"
+#include "stdafx.h"
 #include "RobotInspection.h"
 #include "opencv/highgui.h"
 #include "opencv/cv.h"
@@ -12,17 +10,17 @@ using namespace cv;
 #endif
 
 
-// 唯一的应用程序对象
+// 脦篓脪禄碌脛脫娄脫脙鲁脤脨貌露脭脧贸
 
 CWinApp theApp;
 
 using namespace std;
-typedef int (*Dllfun)(const char* strIpAddr, HWND hWnd, H264STREAMRECV_CB h264StreamCallBack, LIVEVIDEOSTREAMRGBRECV_CB liveVideoStreamRGBCallback, LIVEVIDEOMEASUREDATARECV_CB liveVideoMeasureDataCallBack, LIVEVIDEOSTREAMSTATERECV_CB liveVideoStreamStateCallBack, LIVEVIDEOMEASURESTATERECV_CB liveVideoMeasureStateCallBack);
-typedef int (*Dllfun2)(const char* strIpAddr, HWND hWnd, int palIndex, LIVEVIDEORGBANDMEASUREDATARECV_CB liveVideoRGBAndMeasureDataCallBack, LIVEVIDEOMEASURESTATERECV_CB liveVideoMeasureStateCallBack);
-typedef int (*Dllfun3)(int iSessionID);
-typedef int (*Dllfun4)(const char* filename, int iSessionID);
-bool stop =true;
-Mat contour(Mat& image)
+typedef int(*Dllfun)(const char* strIpAddr, HWND hWnd, H264STREAMRECV_CB h264StreamCallBack, LIVEVIDEOSTREAMRGBRECV_CB liveVideoStreamRGBCallback, LIVEVIDEOMEASUREDATARECV_CB liveVideoMeasureDataCallBack, LIVEVIDEOSTREAMSTATERECV_CB liveVideoStreamStateCallBack, LIVEVIDEOMEASURESTATERECV_CB liveVideoMeasureStateCallBack);
+typedef int(*Dllfun2)(const char* strIpAddr, HWND hWnd, int palIndex, LIVEVIDEORGBANDMEASUREDATARECV_CB liveVideoRGBAndMeasureDataCallBack, LIVEVIDEOMEASURESTATERECV_CB liveVideoMeasureStateCallBack);
+typedef int(*Dllfun3)(int iSessionID);
+typedef int(*Dllfun4)(const char* filename, int iSessionID);
+bool stop = true;
+Mat contour(Mat& image,Mat& mask,int& counter,float& temp)
 {
 	Mat gray;
 	Vec3b p;
@@ -36,6 +34,7 @@ Mat contour(Mat& image)
 	vector<Vec4i> hierarchy;
 	findContours(img, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
 	Mat resultImage = Mat::zeros(img.size(), CV_8U);
+	counter = -1;
 	for (int j = 0; j < contours.size(); j++)
 	{
 		vector<Point> poly;
@@ -43,21 +42,33 @@ Mat contour(Mat& image)
 		Rect r = boundingRect(Mat(poly));
 		int needPoint = 50;
 		int nowPoint = 0;
-		while(nowPoint<needPoint&&poly.size()>10)
+		float temp_t = 0;
+		int counter_t = 0;
+		while (nowPoint<needPoint&&poly.size()>10)
 		{
 			int rx = rand() % (r.width) + r.x;
 			int ry = rand() % (r.height) + r.y;
-			double d=pointPolygonTest(poly, Point2f(rx, ry), false);
+			double d = pointPolygonTest(poly, Point2f(rx, ry), false);
 			if (d > 0) {
 				nowPoint++;
-				p[0] = 255;
+				/*p[0] = 255;
 				p[1] = 0;
 				p[2] = 0;
-				image.at<Vec3b>(ry, rx) = p;
+				image.at<Vec3b>(ry, rx) = p;*/
+				if (mask.at<float>(ry, rx) > 28 && mask.at<float>(ry, rx)<40)
+				{
+					counter_t++;
+				}
+				temp_t += mask.at<float>(ry, rx);
 			}
-
 		}
-
+		temp_t = temp_t / nowPoint;
+		if (counter_t > counter)
+		{
+			counter = counter_t;
+			temp = temp_t;
+		}
+			
 		vector<Point>::const_iterator itp = poly.begin();
 		while (itp != (poly.end() - 1))
 		{
@@ -72,14 +83,15 @@ int CALLBACK rgbcallback(const LiveStream_RGBAndMeasureDataCallBack *pInfo)
 {
 	//return 0;
 	//printf("picture %d",);
-	static int index=0;
-	printf("picture w(%d),h(%d),size(%d,%d),%d",pInfo->ImgWidth,pInfo->ImgHeight,pInfo->RGBDataLen,3*pInfo->ImgWidth*pInfo->ImgHeight,pInfo->matrixDataLen);
-			int row=pInfo->ImgHeight;
-		int col=pInfo->ImgWidth;
-	Mat mask=Mat::zeros(row,col,CV_8U);
+	static int index = 0;
+	printf("picture w(%d),h(%d),size(%d,%d),%d", pInfo->ImgWidth, pInfo->ImgHeight, pInfo->RGBDataLen, 3 * pInfo->ImgWidth*pInfo->ImgHeight, pInfo->matrixDataLen);
+	int row = pInfo->ImgHeight;
+	int col = pInfo->ImgWidth;
+	//Mat mask = Mat::zeros(row, col, CV_8U);
+	Mat mask = Mat::zeros(row, col, CV_32F);
 	char imageName[100];
-		sprintf(imageName, "image/i_%d.jpg", index++);
-	if(pInfo->MeasureMatrix!=NULL)
+
+	if (pInfo->MeasureMatrix != NULL)
 	{
 
 		//float* mem = new float[pInfo->ImgWidth*pInfo->ImgHeight];
@@ -92,18 +104,20 @@ int CALLBACK rgbcallback(const LiveStream_RGBAndMeasureDataCallBack *pInfo)
 		src.convertTo(m,CV_8U,belt,alph);
 		uchar uu=m.at<uchar>(200,200);
 		printf("tem:%d\n,%d",(int)uu,sizeof(float));*/
-		float belt=255.0f/170.0f;
-		for(int i=0;i<row;i++)
+		float belt = 255.0f / 170.0f;
+		for (int i = 0; i<row; i++)
 		{
-			for(int j=0;j<col;j++)
+			for (int j = 0; j<col; j++)
 			{
-				if(pInfo->MeasureMatrix[i*col+j]>20&&pInfo->MeasureMatrix[i*col+j]<40)
+
+				mask.at<float>(i, j) = pInfo->MeasureMatrix[i*col + j];
+				/*if (pInfo->MeasureMatrix[i*col + j]>20 && pInfo->MeasureMatrix[i*col + j]<40)
 				{
-					mask.at<uchar>(i,j)=255;
+					mask.at<uchar>(i, j) = 255;
 					//mask.at<uchar>(i,j)=(pInfo->MeasureMatrix[i*col+j]+20)*belt;
 				}
 				else
-					mask.at<uchar>(i,j)=0;
+					mask.at<uchar>(i, j) = 0;*/
 			}
 		}
 		//printf("tem:%f,%d\n",pInfo->MeasureMatrix[pInfo->ImgWidth*200+200],(int)mask.at<uchar>(200,200));
@@ -115,20 +129,45 @@ int CALLBACK rgbcallback(const LiveStream_RGBAndMeasureDataCallBack *pInfo)
 		//delete [] mem;
 		//return 1;
 	}
-	if(pInfo->pRGBData != NULL)
-	{		
-		
-	
-		BYTE* mem = new BYTE[3*pInfo->ImgWidth*pInfo->ImgHeight];
-		memcpy(mem, pInfo->pRGBData, 3*pInfo->ImgWidth*pInfo->ImgHeight);
-		Mat rgbsrc(row,col,CV_8UC3,mem);
+	if (pInfo->pRGBData != NULL)
+	{
+
+
+		BYTE* mem = new BYTE[3 * pInfo->ImgWidth*pInfo->ImgHeight];
+		memcpy(mem, pInfo->pRGBData, 3 * pInfo->ImgWidth*pInfo->ImgHeight);
+		Mat rgbsrc(row, col, CV_8UC3, mem);
 		//imshow("windows1",src);
 		//waitKey(1);
 		//contour(src);
-		Mat ss(row,col,CV_8UC3);
-		rgbsrc.copyTo(ss,mask);
-		imwrite(imageName,ss);
-		delete [] mem;
+		//Mat ss(row, col, CV_8UC3);
+		//rgbsrc.copyTo(ss, mask);
+		int count = 0;
+		float tem = 0;
+		index++;
+		contour(rgbsrc,mask,count, tem);
+		if (count > 5)
+		{
+			sprintf(imageName, "image/person/i_%d_%d_%.2f.jpg",index ,count,tem);
+			imwrite(imageName, rgbsrc);
+			sprintf(imageName, "image/person/i_%d_%d_%.2f.dat", index, count, tem);
+			FileStorage fs(imageName, FileStorage::WRITE);
+			fs << "vocabulary" << mask;
+			fs.release();
+		}
+		else
+		{
+			sprintf(imageName, "image/no_person/i_%d_%d_%.2f.jpg", index, count, tem);
+			imwrite(imageName, rgbsrc);
+			sprintf(imageName, "image/np_person/i_%d_%d_%.2f.dat", index, count, tem);
+			FileStorage fs(imageName, FileStorage::WRITE);
+			fs << "vocabulary" << mask;
+			fs.release();
+		}
+		
+		
+		
+
+		delete[] mem;
 		//imshow("aa",src);
 		//waitKey(1);
 	}
@@ -150,47 +189,47 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 
 	if (hModule != NULL)
 	{
-		// 初始化 MFC 并在失败时显示错误
+		// 鲁玫脢录禄炉 MFC 虏垄脭脷脢搂掳脺脢卤脧脭脢戮麓铆脦贸
 		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
 		{
-			// TODO: 更改错误代码以符合您的需要
-			_tprintf(_T("错误: MFC 初始化失败\n"));
+			// TODO: 赂眉赂脛麓铆脦贸麓煤脗毛脪脭路没潞脧脛煤碌脛脨猫脪陋
+			_tprintf(_T("麓铆脦贸: MFC 鲁玫脢录禄炉脢搂掳脺\n"));
 			nRetCode = 1;
 		}
 		else
 		{
-			// TODO: 在此处为应用程序的行为编写代码。
+			// TODO: 脭脷麓脣麓娄脦陋脫娄脫脙鲁脤脨貌碌脛脨脨脦陋卤脿脨麓麓煤脗毛隆拢
 			Dllfun2 maopao1;
 			Dllfun3 maopao3;
 			Dllfun4 maopao4;
 			HINSTANCE hdll;
 			//cv::namedWindow("aa");
-			//cv::namedWindow("windows1",0); //创建一个窗口 
-			//HWND hWnd = (HWND)cvGetWindowHandle("windows1");//获取子窗口的HWND
-			hdll=LoadLibrary(L"RobotInspection.dll");
-			if(hdll==NULL)
+			//cv::namedWindow("windows1",0); //麓麓陆篓脪禄赂枚麓掳驴脷 
+			//HWND hWnd = (HWND)cvGetWindowHandle("windows1");//禄帽脠隆脳脫麓掳驴脷碌脛HWND
+			hdll = LoadLibrary(L"RobotInspection.dll");
+			if (hdll == NULL)
 			{
 				FreeLibrary(hdll);
 			}
-			maopao1=(Dllfun2)GetProcAddress(hdll,"LiveStream_OpenDevice");
-			maopao3=(Dllfun3)GetProcAddress(hdll,"LiveStream_CloseDevice");
-			LIVEVIDEORGBANDMEASUREDATARECV_CB rgb=rgbcallback;
-			LIVEVIDEOMEASURESTATERECV_CB stat=statscallback;
-			int iSessionID=maopao1("192.168.1.168",NULL,1,rgb,stat);
-			printf("iSessionID=%d",iSessionID);
+			maopao1 = (Dllfun2)GetProcAddress(hdll, "LiveStream_OpenDevice");
+			maopao3 = (Dllfun3)GetProcAddress(hdll, "LiveStream_CloseDevice");
+			LIVEVIDEORGBANDMEASUREDATARECV_CB rgb = rgbcallback;
+			LIVEVIDEOMEASURESTATERECV_CB stat = statscallback;
+			int iSessionID = maopao1("192.168.1.168", NULL, 1, rgb, stat);
+			printf("iSessionID=%d", iSessionID);
 			//Sleep(10000);
 			//maopao4=(Dllfun4)GetProcAddress(hdll,"LiveStream_Slap");
 			//maopao4("image/aa.jpg",iSessionID);
 			printf("/n/n/n/ndsaf");
-			char input=' ';
-			scanf("%c",&input);
+			char input = ' ';
+			scanf("%c", &input);
 			maopao3(iSessionID);
 		}
 	}
 	else
 	{
-		// TODO: 更改错误代码以符合您的需要
-		_tprintf(_T("错误: GetModuleHandle 失败\n"));
+		// TODO: 赂眉赂脛麓铆脦贸麓煤脗毛脪脭路没潞脧脛煤碌脛脨猫脪陋
+		_tprintf(_T("麓铆脦贸: GetModuleHandle 脢搂掳脺\n"));
 		nRetCode = 1;
 	}
 
